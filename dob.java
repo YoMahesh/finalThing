1ad835005539f0a4db14dffbf97f25fbf0fd0edd
YoMahesh <Maheshsulegai724@gmail.com>
2024-05-28 10:36:30 +0530

    merge conflict

--- dob.java
+++ dob.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
     String dob = "01/01/2005";
-    System.out.println("My date of birth is " + dob);
+    System.out.println("earth landing day is " + dob);
   }
 }
