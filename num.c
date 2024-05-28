ddd2226c92a9739562a1223e08e6f14096a3aaff
MaheshSulegai <156065935+YoMahesh@users.noreply.github.com>
2024-05-28 10:34:16 +0530

    Update num.c

--- num.c
+++ num.c
@@ -3,13 +3,13 @@
 int main() {
     int num1, num2, sum;
     
-    printf("Enter the first number: ");
+    printf("Enter the number: ");
     scanf("%d", &num1);
     
-    printf("Enter the second number: ");
+    printf("Enter the number: ");
     scanf("%d", &num2);
     
-    sum = num1 - num2;
+    sum = num1 + num2;
     
     printf("The sum of %d and %d is %d.", num1, num2, sum);
     
